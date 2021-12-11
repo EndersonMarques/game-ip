0f3bd02f79402a1c1bece092ae9c1a9f31591b84
EndersonMarques <EndersonMarques>
2021-12-11 18:28:08 -0300

    Feito a tela de Menu, e o gerenciamento de telas do jogo :D

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include "include/raylib.h"
 
+// Estrutura de telas
+typedef enum TelaGame
+{
+    LOGO = 0,
+    MENU,
+    GAMEPLAY,
+    FIM
+} TelaGame;
+
 int main(void)
 {
     // Initialization
@@ -7,7 +16,15 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Meu Jogo - Raylib");
+    InitWindow(screenWidth, screenHeight, "Game - IP - CIN");
+
+    Texture2D textura = LoadTexture("assets/banner.png");
+
+    //Tela atual
+    TelaGame telaAtual = LOGO;
+
+    int contador_frame = 0;
+    int life = 100;
 
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -16,6 +33,38 @@ int main(void)
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         // Update
+        switch (telaAtual)
+        {
+        case LOGO:
+            contador_frame++;
+            if (contador_frame > 120)
+            {
+                telaAtual = MENU;
+            }
+            break;
+
+        case MENU:
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                telaAtual = GAMEPLAY;
+            }
+            break;
+        case GAMEPLAY:
+            if (life <= 0 || IsKeyPressed(KEY_ENTER))
+            {
+                telaAtual = FIM;
+            }
+            break;
+        case FIM:
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                telaAtual = MENU;
+            }
+            break;
+
+        default:
+            break;
+        }
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
@@ -25,14 +74,32 @@ int main(void)
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
+        switch (telaAtual)
+        {
+        case LOGO:
+            DrawText("Carregando o Jogo Peste", 200, 200, 35, DARKGREEN);
+            break;
 
-        DrawText("Olá Manu, sua primeira Janela!", 200, 200, 30, DARKGREEN);
+        case MENU:
+            DrawTexture(textura, screenWidth / 2 - textura.width / 2, screenHeight / 2 - textura.height / 2, WHITE);
+            DrawText("Pressione ENTER para iniciar", 280, 350, 20, RED);
+            break;
+        case GAMEPLAY:
+            DrawText("Tela do Jogo", 40, 200, 35, DARKBLUE);
+            break;
+        case FIM:
+            DrawText("MORRESSE BIXO :/", 150, 150, 50, DARKBLUE);
+            break;
+        default:
+            break;
+        }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
+    UnloadTexture(textura);
     //--------------------------------------------------------------------------------------
     CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
